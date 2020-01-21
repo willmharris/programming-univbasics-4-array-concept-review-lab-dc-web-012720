a6c4e6b681f9b3dbca4ee9890302ed88477bd9d7
William Harris <willmharris0@gmail.com>
2020-01-21 03:47:36 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,8 +6,8 @@ def find_element_index(array, value_to_find)
     break if variable == value_to_find
     counter += 1
   end
-  if variable == value_to_find return counter 
-  else return nil 
+  if variable == value_to_find counter 
+  else nil 
 end
 
 
