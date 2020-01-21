565c53f7b5e2d0684fabfef6a7d3e1f18a777d81
William Harris <willmharris0@gmail.com>
2020-01-21 03:30:41 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,7 +10,9 @@ end
 
 while count < 10 do 
   break if count == magic_exit_number 
-  puts "I am the #{count}, I love to count!" # Work count = count + 1 end
+  puts "I am the #{count}, I love to count!"
+  count = count + 1 
+end
 
 
 
