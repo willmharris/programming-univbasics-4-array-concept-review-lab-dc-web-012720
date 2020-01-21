fb3d914453e39137278b41c4edff6a71d52640c3
William Harris <willmharris0@gmail.com>
2020-01-21 03:35:12 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,9 +8,10 @@ def find_element_index(array, value_to_find)
   end
 end
 
-magic_exit_number = 7
+
 count = 0 
-while count < 10 do 
+while count < 10 do
+  magic_exit_number = 7
   break if count == magic_exit_number 
   puts "I am the #{count}, I love to count!"
   count = count + 1 
